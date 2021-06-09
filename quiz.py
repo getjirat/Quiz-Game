3d903088ad214ea28d000327c2547254ff0dfdcb
getjirat <69470152+getjirat@users.noreply.github.com>
2021-06-09 17:34:53 +0700

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,1 +1,10 @@
-
+import pickle
+def _init_():
+  question_lists = ["",""]
+  answer_lists = ["",""]
+  question_lists = pickle.load(open("assets/data/question-lists.txt","rb"))
+  answer_lists = pickle.load(open("assets/data/answer-lists.txt","rb"))
+  print(question_lists)
+  print(answer_lists)
+_init_()
+  
